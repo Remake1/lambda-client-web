c33bfbefcb458631c72fae08cf78afdf76558585
Remake1 <deerteeu@gmail.com>
2025-11-19 21:49:03 -0600

    Implemented GuestGuard

---
+++ src/components/auth/GuestGuard.tsx
@@ -0,0 +1,21 @@
+import { type ReactNode } from 'react';
+import { Navigate, useLocation } from 'react-router';
+import { useSelector } from 'react-redux';
+import { type RootState } from '@/store/store';
+
+interface GuestGuardProps {
+    children: ReactNode;
+}
+
+const GuestGuard = ({ children }: GuestGuardProps) => {
+    const { isAuthenticated } = useSelector((state: RootState) => state.auth);
+    const location = useLocation();
+
+    if (isAuthenticated) {
+        return <Navigate to="/account" state={{ from: location }} replace />;
+    }
+
+    return <>{children}</>;
+};
+
+export default GuestGuard;
